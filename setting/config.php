323027f6d57f098f8c0efb7f19e983fafa4c4be8
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-16 21:37:55 +0900

    00030001[add]新規ファイル追加
    新規のコミットです。

---
+++ setting/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+  config.php
+  設定ファイル
+
+*/
+
+
+/* DB接続情報（MySQL） */
+// ローカル
+$host = 'localhost';
+$dbname = 'osk';
+$user = 'root';
+$password = 'ryugo1120';
+
+$dbh = new PDO('mysqql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $password);
+
+define('PASSWORD_KEY', 'EO&(ZOM&'); // 暗号化キー（8桁のパスワード）
+
+error_reporting(E_ALL & ~E_NOTICE);
