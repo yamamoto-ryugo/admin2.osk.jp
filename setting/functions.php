e985b1fadd2127b9214948b2405bd9b89d99a1ee
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-17 20:07:32 +0900

    00040007[update]機能修正
    関数ファイルに，トークンに関する関数を追加しました。

--- setting/functions.php
+++ setting/functions.php
@@ -1,5 +1,19 @@
 <?php
 
+// 文字列に関する関数
 function h($s) {
   return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
+// Tokenに関する関数
+function setToken() {
+  $token = sha1(uniqid(mt_rand(), true));
+  $_SESSION['token'] = $token;
+}
+
+function checkToken() {
+  if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'])) {
+    echo "不正アクセスが行われました。";
+    exit;
+  }
 }
