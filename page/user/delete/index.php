e13ab8f5faf58958428df17fc36afdccb0d3f3e1
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-20 17:58:27 +0900

    00030018[add]新規ファイル追加
    ユーザー削除ページを追加しました。

---
+++ page/user/delete/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../../../setting/config.php');
+require_once('../../../setting/functions.php');
+require_once('../../../setting/version.php');
+
+session_start();
+
+if (empty($_SESSION['me'])) {
+  header('Location: ../../../page/login/');
+  exit;
+}
+
+$me = $_SESSION['me'];
+
+if ($me['admin'] == 1) {
+  header('Location: ../../../');
+  exit;
+}
+
+$id = (int) $_GET['id'];
+
+$sql = "DELETE FROM user WHERE id = ?";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(1, $id, PDO::PARAM_INT);
+$stmt->execute();
+
+$dbh = null;
+
+header("Location: ../");
+exit;
